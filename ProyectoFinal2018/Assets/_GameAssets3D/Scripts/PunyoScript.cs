0364f1c01b946c3b81585eef993bbbec719aff1b
oscar <oscarcurso1976@gmail.com>
2018-12-10 13:41:59 +0100

    +

--- ProyectoFinal2018/Assets/_GameAssets3D/Scripts/PunyoScript.cs
+++ ProyectoFinal2018/Assets/_GameAssets3D/Scripts/PunyoScript.cs
@@ -5,13 +5,24 @@ using UnityEngine.AI;
 
 public class PunyoScript : MonoBehaviour {
 
-    public Animator elvisAnimator;
-    public NavMeshAgent agenteElvis;
-    public Animator abuelaAnimator;
-    public NavMeshAgent agenteAbuela;
-    public Animator chicaAnimator;
-    public NavMeshAgent agenteChica;
-
+    public Animator elvisAnimator1;
+    public NavMeshAgent agenteElvis1;
+    public Animator elvisAnimator2;
+    public NavMeshAgent agenteElvis2;
+    public Animator elvisAnimator3;
+    public NavMeshAgent agenteElvis3;
+    public Animator abuelaAnimator1;
+    public NavMeshAgent agenteAbuela1;
+    public Animator abuelaAnimator2;
+    public NavMeshAgent agenteAbuela2;
+    public Animator abuelaAnimator3;
+    public NavMeshAgent agenteAbuela3;
+    public Animator chicaAnimator1;
+    public NavMeshAgent agenteChica1;
+    public Animator chicaAnimator2;
+    public NavMeshAgent agenteChica2;
+    public Animator chicaAnimator3;
+    public NavMeshAgent agenteChica3;
     void Start () {
 		
 	}
@@ -23,25 +34,69 @@ public class PunyoScript : MonoBehaviour {
 
     private void OnTriggerEnter(Collider collider) {
 
-        if (collider.gameObject.name == "Elvis") {
-            print("colision Elvis");
+        if (collider.gameObject.name == "Elvis1") {
+            print("colision Elvis1");
 
-            elvisAnimator.SetBool("ostiado", true);
-            agenteElvis.Stop();
+            elvisAnimator1.SetBool("ostiado", true);
+            agenteElvis1.Stop();
 
         }
+        if (collider.gameObject.name == "Elvis2") {
+            print("colision Elvis2");
+
+            elvisAnimator2.SetBool("ostiado", true);
+            agenteElvis2.Stop();
+
+        }
+        if (collider.gameObject.name == "Elvis3") {
+            print("colision Elvis3");
+
+            elvisAnimator3.SetBool("ostiado", true);
+            agenteElvis3.Stop();
+
+        }
+
+        
         if (collider.gameObject.name == "Abuela1") {
-            print("colision Abuela");
+            print("colision Abuela1");
 
-            abuelaAnimator.SetBool("ostiado", true);
-            agenteAbuela.Stop();
+            abuelaAnimator1.SetBool("ostiado", true);
+            agenteAbuela1.Stop();
 
         }
-        if (collider.gameObject.name == "Chica") {
-            print("colision Chica");
+        if (collider.gameObject.name == "Abuela2") {
+            print("colision Abuela2");
 
-            chicaAnimator.SetBool("ostiado", true);
-            agenteChica.Stop();
+            abuelaAnimator2.SetBool("ostiado", true);
+            agenteAbuela2.Stop();
+
+        }
+        if (collider.gameObject.name == "Abuela3") {
+            print("colision Abuela3");
+
+            abuelaAnimator3.SetBool("ostiado", true);
+            agenteAbuela3.Stop();
+
+        }
+        if (collider.gameObject.name == "Chica1") {
+            print("colision Chica1");
+
+            chicaAnimator1.SetBool("ostiado", true);
+            agenteChica1.Stop();
+
+        }
+        if (collider.gameObject.name == "Chica2") {
+            print("colision Chica2");
+
+            chicaAnimator2.SetBool("ostiado", true);
+            agenteChica2.Stop();
+
+        }
+        if (collider.gameObject.name == "Chica3") {
+            print("colision Chica3");
+
+            chicaAnimator3.SetBool("ostiado", true);
+            agenteChica3.Stop();
 
         }
     } 
