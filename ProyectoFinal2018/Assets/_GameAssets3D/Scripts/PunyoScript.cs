b88ef313e48a5cab407bdad6bec2851161cc965b
oscar <oscarcurso1976@gmail.com>
2018-12-11 13:46:52 +0100

    +

--- ProyectoFinal2018/Assets/_GameAssets3D/Scripts/PunyoScript.cs
+++ ProyectoFinal2018/Assets/_GameAssets3D/Scripts/PunyoScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class PunyoScript : MonoBehaviour {
 
@@ -25,6 +26,12 @@ public class PunyoScript : MonoBehaviour {
     public NavMeshAgent agenteChica2;
     public Animator chicaAnimator3;
     public NavMeshAgent agenteChica3;
+
+    public Text txtvida;
+    public Text txtPuntos;
+    private int puntos = 0;
+
+
     void Start () {
 		
 	}
@@ -38,21 +45,21 @@ public class PunyoScript : MonoBehaviour {
 
         if (collider.gameObject.name == "Elvis1") {
             print("colision Elvis1");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             elvisAnimator1.SetBool("ostiado", true);
             agenteElvis1.isStopped =true;
 
         }
         if (collider.gameObject.name == "Elvis2") {
             print("colision Elvis2");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             elvisAnimator2.SetBool("ostiado", true);
             agenteElvis2.isStopped = true;
 
         }
         if (collider.gameObject.name == "Elvis3") {
             print("colision Elvis3");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             elvisAnimator3.SetBool("ostiado", true);
             agenteElvis3.isStopped = true;
 
@@ -61,49 +68,49 @@ public class PunyoScript : MonoBehaviour {
         
         if (collider.gameObject.name == "Abuela1") {
             print("colision Abuela1");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             abuelaAnimator1.SetBool("ostiado", true);
             agenteAbuela1.isStopped = true;
 
         }
         if (collider.gameObject.name == "Abuela2") {
             print("colision Abuela2");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             abuelaAnimator2.SetBool("ostiado", true);
             agenteAbuela2.isStopped = true;
 
         }
         if (collider.gameObject.name == "Abuela3") {
             print("colision Abuela3");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             abuelaAnimator3.SetBool("ostiado", true);
             agenteAbuela3.isStopped = true;
 
         }
         if (collider.gameObject.name == "Abuela4") {
             print("colision Abuela4");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             abuelaAnimator4.SetBool("ostiado", true);
             agenteAbuela4.isStopped = true;
 
         }
         if (collider.gameObject.name == "Chica1") {
             print("colision Chica1");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             chicaAnimator1.SetBool("ostiado", true);
             agenteChica1.isStopped = true;
 
         }
         if (collider.gameObject.name == "Chica2") {
             print("colision Chica2");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             chicaAnimator2.SetBool("ostiado", true);
             agenteChica2.isStopped = true;
 
         }
         if (collider.gameObject.name == "Chica3") {
             print("colision Chica3");
-
+            txtPuntos.text = "Puntos: " + puntos++;
             chicaAnimator3.SetBool("ostiado", true);
             agenteChica3.isStopped = true;
 
