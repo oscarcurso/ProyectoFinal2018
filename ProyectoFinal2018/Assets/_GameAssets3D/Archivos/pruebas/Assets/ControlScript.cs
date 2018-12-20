a06ac06f93d139ffea1fb8a0f35a4f6281199076
oscar <oscarcurso1976@gmail.com>
2018-12-20 12:47:58 +0100

    subidaPruebaVR

---
+++ ProyectoFinal2018/Assets/_GameAssets3D/Archivos/pruebas/Assets/ControlScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScript : MonoBehaviour {
+
+
+    int speedRotarParado = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Vertical") > 0.1f) {
+            transform.Translate(0, 0, 1);
+           
+        } else {
+
+           
+        }
+
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * speedRotarParado, 0);
+
+    }
+
+
+}
+
+       
+
